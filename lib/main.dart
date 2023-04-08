ee68ea15de8884fb5a3ae5841c79d72c6ec3b0b2
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-08 16:52:28 -0300

    Chart Widget #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:despesas_pessoais__flutter/components/chart.dart';
 import 'package:despesas_pessoais__flutter/components/transaction_form.dart';
 import 'package:despesas_pessoais__flutter/components/transaction_list.dart';
 import 'package:despesas_pessoais__flutter/models/transaction.dart';
@@ -48,19 +49,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'Tenis Corrida',
-    //   value: 310.00,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Conta de Luza',
-    //   value: 120.30,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: 't0',
+      title: 'Aluguel',
+      value: 500.0,
+      date: DateTime.now().subtract(Duration(days: 33)),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Tenis Corrida',
+      value: 310.00,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luza',
+      value: 120.30,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
   ];
+
+  // Passa as transações rencentes para o comp
+  List<Transaction> get _recentTransactions {
+    // Filter  / Retorna uma lista
+    return _transactions.where((tr) {
+      //Pega a data de agr e subtrai 7 dias
+      return tr.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
 
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
@@ -103,13 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Card(
-                child: Text('Gráfico'),
-                elevation: 5,
-                color: Colors.cyan,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions),
           ],
         ),
