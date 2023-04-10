0a04be803d0bb1af656fd14eb37faa59aa87fe85
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-10 16:39:23 -0300

    Objeto Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -102,8 +102,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscap =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscap = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text(
@@ -128,9 +128,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // Altura inicial da aplicacao
     // Removendo altura da appBar e da StatusBar da altura total
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
